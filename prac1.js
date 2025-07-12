14812be7a1988b4438d19231e60c5d50e1b15ac4
priya-garg-coder <pg0052761@gmail.com>
2025-07-12 00:34:20 -0700

    basic Express server

---
+++ prac1.js
@@ -0,0 +1,14 @@
+// const express = require('express')
+// const app = express()                                                       
+// const port = 3000
+// app.set("view engine","ejs");
+
+// app.get('/', (req, res) => {
+//   logoname="flipkart"
+//   brandname="amazon"
+//   console.log("helo world")
+//   res.render("index",{logoname:logoname, brandname:brandname});
+// })
+// app.listen(port, () => {
+//    console.log(`Example app listening on port ${port}`)
+//  })
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()                                                       
+const port = 3000
+app.set("view engine","ejs");
+
+app.get('/', (req, res) => {
+  logoname="flipkart"
+  brandname="amazon"
+  res.render("index",{logoname:logoname, brandname:brandname});
+})
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`)
+ })

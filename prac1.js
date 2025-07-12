5fe1be6db43e421d2da422f11b1d66ecd63bbb61
priya-garg-coder <pg0052761@gmail.com>
2025-07-12 01:18:05 -0700

    add static file

--- prac1.js
+++ prac1.js
@@ -1,14 +1,26 @@
 // const express = require('express')
-// const app = express()                                                       
+// const app = express()
 // const port = 3000
-// app.set("view engine","ejs");
 
 // app.get('/', (req, res) => {
-//   logoname="flipkart"
-//   brandname="amazon"
-//   console.log("helo world")
-//   res.render("index",{logoname:logoname, brandname:brandname});
+//   res.send("hello guys")
+//   console.log("hello world")
 // })
 // app.listen(port, () => {
-//    console.log(`Example app listening on port ${port}`)
-//  })
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+  res.send("hello guys")
+  console.log("hello world")
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+

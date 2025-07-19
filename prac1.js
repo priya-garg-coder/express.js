3a6c40a08dde6695b6243511c29ae56463631106
priya-garg-coder <pg0052761@gmail.com>
2025-07-19 00:50:25 -0700

    add middleware

--- prac1.js
+++ prac1.js
@@ -9,22 +9,43 @@
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
 // })
-const express = require('express');
-const app = express();
-const port = 4000;
-app.use(express.static("public"))
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// app.use(express.static("public"))
 
-app.get("/",(req,res)=>{
-    console.log("hello world");
-    res.send("hello");
-}).put("/ok",(req,res)=>{
-  console.log("hello world")
-  res.sendFile("public/prac.html",{root:__dirname})
+// app.get("/",(req,res)=>{
+//     console.log("hello world");
+//     res.send("hello");
+// }).put("/ok",(req,res)=>{
+//   console.log("hello world")
+//   res.sendFile("public/prac.html",{root:__dirname}) //serving html file
     
-}).post("/",(req,res)=>{
-    console.log("hello");
-    res.send("hello word");
-})
+// }).post("/",(req,res)=>{
+//     console.log("hello");
+//     res.send("hello word");
+// })
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+// const express = require("express")
+// const article = require("./routes/article")
+// app.use("/article",article);
+
+// let port = 3000
+// app = express()
+// app.use(express.static("public"))
+
+const express = require("express");
+const hello = require("./routes/hello");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use("/hello", hello);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server is running at http://localhost:${port}`);
+});
